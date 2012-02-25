c8862c7e5d114fc02b467ac7e76d3ee2c831339c
Guido Flohr <guido.flohr@cantanea.com>
2012-02-25 14:21:19 +0200

    Stubs for all FIBS commands
    TODO: Somehow handle the !! command specially.  We cannot create a file
    !!.pm.

---
+++ lib/OpenFIBS/Server/Command/version.pm
@@ -0,0 +1,41 @@
+#! /bin/false
+
+# This file is part of OpenFIBS.
+# Copyright (C) 2012 Guido Flohr, http://guido-flohr.net/.
+#
+# OpenFIBS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenFIBS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OpenFIBS.  If not, see <http://www.gnu.org/licenses/>.
+
+package OpenFIBS::Server::Command::version;
+
+use strict;
+
+use base qw (OpenFIBS::Server::Command);
+
+1;
+
+=head1 NAME
+
+OpenFIBS::Server::Command::version - OpenFIBS Command `version'
+
+=head1 SYNOPSIS
+
+  use OpenFIBS::Server::Command::version->new (version => $session, $call);
+  
+=head1 DESCRIPTION
+
+This plug-in handles the ommand `version'.
+
+=head1 SEE ALSO
+
+OpenFIBS::Server::Command(3pm), openfibs(1), perl(1)
