44ea6b2843f3b0efd74ca7e745025af562c11ab7
Guido Flohr <guido.flohr@cantanea.com>
2012-03-03 13:29:16 +0200

    Implementation for tell/tellx

--- lib/BaldLies/Session/Command/tell.pm
+++ lib/BaldLies/Session/Command/tell.pm
@@ -22,8 +22,61 @@ use strict;
 
 use base qw (BaldLies::Session::Command);
 
+use BaldLies::Util qw (empty);
+
 sub aliases {
     return 'tellx', 'tell';
+}
+
+sub execute {
+    my ($self, $payload) = @_;
+    
+    my $session = $self->{_session};
+    
+    my ($recipient, $message) = split / /, $payload, 2;
+
+    if (empty $recipient) {
+        $session->reply ("** Tell whom?\n");
+        return $self;
+    }
+    if (empty $message) {
+        $session->reply ("** Tell $recipient what?\n");
+        return $self;
+    }
+    
+    my $users = $session->getUsers;
+    if (!exists $users->{$recipient}) {
+        if ('tellx' eq $self->{_call}) {
+            $session->reply ("** There is no one called $recipient\n");
+            return $self;
+        }
+        # Try to match.
+        my @candidates;
+        my $l = length $recipient;
+        foreach my $name (keys %$users) {
+            push @candidates, $name if $recipient eq substr $name, 0, $l;
+        }
+        if (!@candidates) {
+            $session->reply ("** There is no one called $recipient\n");
+            return $self;
+        }
+        if (@candidates != 1) {
+            my $reply = "Ambigous name: Try one of: ";
+            $reply .= join ', ', @candidates;
+            $reply .= "\n** There is no one called $recipient\n";
+            $session->reply ($reply);
+            return $self;
+        }
+        $recipient = $candidates[0];
+    }
+    
+    # TODO! Check whether we have gagged recipient or recipient has gagged us.
+
+    $message .= "\n";
+    $session->clipTell ($recipient, 12, $recipient, $message);
+    $session->clipReply (16, $recipient, $message);
+    
+    return $self;
 }
 
 1;
