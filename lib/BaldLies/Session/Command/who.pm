1f7f47a69a1cb251351a264c70ef43ffa722a329
Guido Flohr <guido.flohr@cantanea.com>
2012-05-03 23:00:17 +0300

    Almost full implementation for who

--- lib/BaldLies/Session/Command/who.pm
+++ lib/BaldLies/Session/Command/who.pm
@@ -23,31 +23,77 @@ use strict;
 use base qw (BaldLies::Session::Command);
 
 use POSIX qw (strftime);
+use BaldLies::Util qw (empty equals);
 
 sub execute {
     my ($self, $payload) = @_;
     
     my $session = $self->{_session};
     my $clip = $session->getClip;
+    my $users = $session->getUsers;
+    
+    if (!$clip && equals 'count', $payload) {
+        my $count = keys %$users;
+        $session->reply ("There are $count users logged on.\n");
+        return $self;
+    }
+    
+    my %users;
+    my $count = 0;
+    foreach my $name (keys %$users) {
+        $users{++$count} = $users->{$name};
+    }
+    
+    if (equals 'ready', $payload) {
+        foreach my $i (keys %users) {
+            my $user = $users{$i};
+            if (!(empty $user->{playing} && $user->{ready})) {
+                delete $users{$i};
+            }
+        }
+    } elsif (equals 'playing', $payload) {
+        foreach my $i (keys %users) {
+            my $user = $users{$i};
+            if (empty $user->{playing}) {
+                delete $users{$i};
+            }
+        }
+    } elsif (equals 'away', $payload) {
+        # Away is not implemented.
+    } elsif (!empty $payload && $payload =~ /^from[ \t]+(.+)$/) {
+        my $where = $1;
+        foreach my $i (keys %users) {
+            my $user = $users{$i};
+            if (0 > index $user->{ip}, $where) {
+                delete $users{$i};
+            }
+        }
+    }
     
     if ($clip) {
-        $self->__showClipWho ($payload);
+        $self->__showClipWho (\%users, equals 'count', $payload);
     } else {
-        $self->__showTelnetWho ($payload);
+        $self->__showTelnetWho (\%users);
     }
     
     return $self;
 }
 
 sub __showClipWho {
-    my ($self) = @_;
+    my ($self, $users, $funny_clip) = @_;
     
     my $session = $self->{_session};
-    my $users = $session->getUsers;
     
     my $output = '';
-    while (my ($name, $user) = each %$users) {
+    foreach my $count (sort { $a <=> $b } keys %$users) {
+        my $user = $users->{$count};
         $output .= '5 ' . $user->rawwho . "\n";
+    }
+    
+    if ($funny_clip) {
+        # Yes, this is what FIBS gives you for the argument `count' in
+        # CLIP mode.
+        $output .= "There are 0 users logged on.\n";
     }
     
     $session->reply ($output);
@@ -56,22 +102,37 @@ sub __showClipWho {
 }
 
 sub __showTelnetWho {
-    my ($self) = @_;
+    my ($self, $users) = @_;
 
     my $session = $self->{_session};
-    my $users = $session->getUsers;
     
     my $output = <<EOF;
 No  S  username        rating   exp login  idle from
 EOF
 
-    my $count = 0;
-    while (my ($name, $user) = each %$users) {
-        my $status = '-';
+    foreach my $count (sort { $a <=> $b } keys %$users) {
+        my $user = $users->{$count};
+        my $s;
+        if (!empty $user->{playing}) {
+            $s = 'P';
+        } elsif ($user->{ready}) {
+            $s = 'R';
+        } else {
+            $s = '-';
+        }
+        my $w;
+        if (!empty $user->{watching}) {
+            $w = 'W';
+        } else {
+            $w = ' ';
+        }
+        # Away is not implemented.
+        my $a = ' ';
         my $login = strftime '%H:%M', gmtime $user->{login};
         my $ip = $user->{ip};
-        $output .= sprintf "\%2d $status  \%-15s \%6.2f %5d $login  00:00 $ip\n",
-                           ++$count, $name, $user->{rating}, $user->{experience};
+        $output .= sprintf "\%2d $s$w$a\%-15s \%6.2f %5d $login  00:00 $ip\n",
+                           $count, $user->{name}, $user->{rating}, 
+                           $user->{experience};
     }
 
     $session->reply ($output);
