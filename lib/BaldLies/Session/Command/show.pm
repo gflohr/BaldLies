ebf2e4b77ed29761f98c566db794787985c0d907
Guido Flohr <guido.flohr@cantanea.com>
2012-05-17 01:24:40 +0300

    Partly implemented show command

--- lib/BaldLies/Session/Command/show.pm
+++ lib/BaldLies/Session/Command/show.pm
@@ -22,6 +22,70 @@ use strict;
 
 use base qw (BaldLies::Session::Command);
 
+use BaldLies::Util qw (empty);
+
+sub execute {
+    my ($self, $payload) = @_;
+    
+    my $session = $self->{_session};
+
+    if (empty $payload) {
+        $session->reply ("** show what?\n");
+        return $self;
+    }
+    
+    my ($what, $argument) = split / +/, $payload;
+    
+    if ('saved' eq $what || 'games' eq $what) {
+        $session->sendMaster (show => $what);
+    } elsif ('savedcount' eq $what) {
+        if (empty $argument) {
+            $session->sendMaster (show => 'savedcount');
+        } else {
+            $session->sendMaster (show => 'savedcount', $argument);
+        }
+    } elsif ('watchers' eq $what) {
+        return $self->__showWatchers;
+    } elsif ('max' eq $what) {
+        $session->reply ("max_logins is 999999 (maximum: 999999)\n");
+        return $self;
+    } else {
+        $session->reply ("** Don't know how to show $what\n");
+        return $self;
+    }
+
+    return $self;
+}
+
+sub __showWatchers {
+    my ($self) = @_;
+    
+    my $session = $self->{_session};
+
+    my $users = $session->getUsers;
+
+    my %watchers;
+    while (my ($name, $user) = each %$users) {
+        next if empty $user->{watching};
+        
+        $watchers{$name} = $user->{watching};
+    }
+    
+    if (!%watchers) {
+        $session->reply ("Watching players: none.\n");
+        return $self;
+    }
+    
+    my $msg = "Watching players:\n";
+    while (my ($watcher, $watchee) = each %watchers) {
+        $msg .= "$watcher is watching $watchee.\n";
+    }
+    
+    $session->reply ($msg);
+    
+    return $self;    
+}
+
 1;
 
 =head1 NAME
